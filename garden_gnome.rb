0d6156fce81341d8f322a03edbde183850e31b2f
meganmersch <meganrm8@gmail.com>
2018-07-26 19:03:14 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,37 @@
 # Code your instances here
+class GardenGnome
+  
+  def name =(name)
+    @name = name
+  end
+  
+  def age =(age)
+    @age = age
+  end
+  
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy = gluten_allergy
+  end
+  
+  def personality =(personality)
+    @personality = evil
+  end
+  
+  def hat_color =(hat_color)
+    @hat_color = hat_color
+  end
+  
+  def gnaw = gnaw(gnaw)
+    @gnaw = "Gnawing on a tree!!!"
+  end
+  
+  def shout = shout(shout)
+    @shout = "GNARLY!!!"
+  end
+  
+  def introduce_self = introduce_self(introduce_self)
+    @introduce_self = "Hello humans, my name is #"
+  
+  
+  
+  
