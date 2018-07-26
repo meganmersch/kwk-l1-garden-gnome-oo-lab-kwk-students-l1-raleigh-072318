c37810c6f5c4164a65cafd2a3af18c58ac0ccbe4
meganmersch <meganrm8@gmail.com>
2018-07-26 19:36:20 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -45,7 +45,10 @@ class GardenGnome
   end
   
   def introduce_self(name, age)
-    "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"  
+    "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end
+  def introduce_self
+    @introduce_self
   end
 end
 
